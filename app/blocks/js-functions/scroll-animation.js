723e626fa7d725f35551bafd1fa6c479e30e0404
Allkrain <aukra25@yandex.ru>
2019-10-02 17:23:04 +0300

    animations

--- app/blocks/js-functions/scroll-animation.js
+++ app/blocks/js-functions/scroll-animation.js
@@ -12,9 +12,79 @@ export default function scrollAnimation() {
     scale: 1,
     delay: 0,
     easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)',
-    duration: 1000,
+    duration: 700,
     cleanup: true,
   });
+
+  if ($('.js-sr_fade').length) {
+    sr.reveal('.js-sr_fade', {
+      interval: 100,
+      opacity: 0,
+    });
+  }
+
+  if ($('.js-sr_fade_d100').length) {
+    sr.reveal('.js-sr_fade_d100', {
+      delay: 100,
+      interval: 100,
+      opacity: 0,
+    });
+  }
+
+  if ($('.js-sr_fade_d200').length) {
+    sr.reveal('.js-sr_fade_d200', {
+      delay: 200,
+      interval: 100,
+      opacity: 0,
+    });
+  }
+
+  if ($('.js-sr_fade_d600').length) {
+    sr.reveal('.js-sr_fade_d600', {
+      delay: 600,
+      opacity: 0,
+      beforeReveal(el) {
+        el.classList.add('is-show');
+      },
+      afterReveal(el) {
+        el.classList.remove('is-show');
+      }
+    });
+  }
+
+  if ($('.js-sr_fade_d800').length) {
+    sr.reveal('.js-sr_fade_d800', {
+      delay: 800,
+      interval: 100,
+      opacity: 0,
+    });
+  }
+
+  if ($('.js-sr_bottom_d700').length) {
+    sr.reveal('.js-sr_bottom_d700', {
+      delay: 700,
+      distance: '30px',
+      origin: 'bottom',
+    });
+  }
+
+  if ($('.js-sr_bottom_d900').length) {
+    sr.reveal('.js-sr_bottom_d900', {
+      delay: 900,
+      interval: 100,
+      distance: '30px',
+      origin: 'bottom',
+    });
+  }
+
+  if ($('.js-sr_bottom_d200').length) {
+    sr.reveal('.js-sr_bottom_d200', {
+      delay: 200,
+      interval: 100,
+      distance: '30px',
+      origin: 'bottom',
+    });
+  }
 
   if ($('.js-sr_bottom').length) {
     sr.reveal('.js-sr_bottom', {
@@ -29,6 +99,31 @@ export default function scrollAnimation() {
       interval: 100,
       distance: '30px',
       origin: 'left',
+    });
+  }
+
+  if ($('.js-sr_right_d200').length) {
+    sr.reveal('.js-sr_right_d200', {
+      delay: 200,
+      interval: 100,
+      distance: '30px',
+      origin: 'right',
+      beforeReveal(el) {
+        el.classList.add('is-show');
+      },
+      afterReveal(el) {
+        el.classList.remove('is-show');
+      }
+    });
+  }
+
+  if ($('.js-sr_right_d500').length) {
+    sr.reveal('.js-sr_right_d500', {
+      delay: 500,
+      interval: 100,
+      distance: '30px',
+      origin: 'right',
+      duration: 1500,
     });
   }
 
