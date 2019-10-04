d54ba37ed53c7a8a997006c41f5c031da33e09bd
Allkrain <aukra25@yandex.ru>
2019-10-04 13:59:19 +0300

    add mobile speed

--- app/components/lead/lead.js
+++ app/components/lead/lead.js
@@ -9,14 +9,17 @@ export default function parallax() {
   });
 
   function castParallax() {
+    const bp = window.globalOptions.sizes;
+
     window.addEventListener('scroll', () => {
+      const wW = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
       const top = window.pageYOffset;
       const layers = document.getElementsByClassName('parallax');
       let layer;
       let speed;
       for (let i = 0; i < layers.length; i += 1) {
         layer = layers[i];
-        speed = layer.getAttribute('data-speed');
+        speed = (wW < bp.sm) ? layer.getAttribute('data-mobile-speed') : layer.getAttribute('data-speed');
         const yPos = -((top * speed) / 100);
         layer.setAttribute('style', `transform: translate3d(0px, ${yPos}px, 0px)`);
       }
